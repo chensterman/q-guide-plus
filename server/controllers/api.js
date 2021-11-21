06e780f9abbd5f20becbaecee68c35dd389535b9
Leon Chen <lrchen01@gmail.com>
2021-11-20 21:57:43 -0500

    updated querying

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -9,32 +9,85 @@ module.exports = class API {
 
     // Querying API function
     static async query(req, res) {
-        // Initialize empty query
-        const matches = {};
+        const regex = {1: [".*", "."]};
+        const text = {2: ""};
+        const range = {
+            "min": {3: {"$gt": ""}},
+            "max": {4: {"$lt": ""}},
+        };
+        const filterMap = {
+            "cs": {
+                "c": regex,
+                "t": regex,
+                "i": regex,
+                "d": text,
+                "te": text,
+            },
+            "cst": {
+                "e": range,
+                "r": range,
+            },
+            "cr": {
+                "o": range,
+                "m": range,
+                "a": range,
+                "f": range,
+                "s": range,
+            },
+            "ir": {
+                "o": range,
+                "l": range,
+                "a": range,
+                "e": range,
+                "en": range,
+                "f": range,
+                "r": range,
+            },
+            "ce": {
+                "w": range,
+                "r": range,
+                "s": range,
+            },
+        };
+        // Initialize empty list for $and query combination
+        const query = [];
 
         // Get query parameters from URL and loop through them
-        const query = req.query;
-        for (const key in query) {
-            // For each matching parameter add the corresponding query to matches
-            if (key.startsWith("courseSummary")) {
-                matches[key] = query[key];
+        const params = req.query;
+        for (const param in params) {
+            var querySingle;
+            const paramArray = param.split(".");
+            for (const str in paramArray) {
+                querySingle = filterMap[str];
+            };
+            if (1 in querySingle) {
+                const regexStr = querySingle[1][0] + params[param] + querySingle[1][1];
+                querySingle = {param: {"$regex": regexStr, "$options": "i"}};
+                query.push(querySingle);
             }
-            else if (key.startsWith("courseRating")) {
-                matches[key] = {"$gt": query[key]};
+            else if (2 in querySingle) {
+                querySingle[2] = params[param];
+                querySingle = {param: querySingle[2]};
+                query.push(querySingle);
             }
-            else if (key.startsWith("instructorRating")) {
-                matches[key] = {"$gt": query[key]};
+            else if (3 in querySingle) {
+                querySingle = querySingle[3];
+                querySingle["$gt"] = params[param];
+                const field = paramArray[0] + "." + paramArray[1];
+                querySingle = {field: querySingle};
+                query.push(querySingle);
             }
-            else if (key.startsWith("courseEval.workload")) {
-                matches[key] = {"$lt": query[key]};
+            else if (4 in querySingle) {
+                querySingle = querySingle[4];
+                querySingle["$lt"] = params[param];
+                const field = paramArray[0] + "." + paramArray[1];
+                querySingle = {field: querySingle};
+                query.push(querySingle);
             }
-            else if (key.startsWith("courseEval")) {
-                matches[key] = {"$gt": query[key]};
-            }
-        }
+        };
         try {
             // Make the final query
-            const courses = await course.find(matches);
+            const courses = await course.find({"$and": query});
             res.status(200).send(courses);
         } catch(e) {
             res.status(400).json({ message: e.message} );
